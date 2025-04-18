78ccb3cabcc4ab452fbcc2e4bff9a685ec89080b
Omprakash Prajapat <omprakashprjpt118@gmail.com>
2025-04-18 17:41:48 +0530

    Create api error,response,handler files

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,26 @@
+const e = require("express");
+
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errros = [],
+    stack = "",
+  ){
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.errors = errros;
+    this.success = false;
+
+    if(stack) {
+      this.stack = stack;
+    }else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+
+module.exports = ApiError;
